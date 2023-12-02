318d5717d5bc0232842c898369e8c0c8f1163c89
TKartist <rimh@usi.ch>
2023-12-02 18:43:59 +0100

    completed scraping the data

---
+++ project/classic_cars/classic_cars/spiders/content_scraper.py
@@ -0,0 +1,35 @@
+import jsonlines
+import scrapy
+from pathlib import Path
+
+
+
+class contentSpider(scrapy.Spider):
+
+    name = "contents"    # Your spider name. Each instance of a QuoteSpider will share the same name.
+    start_urls = []
+    print(Path.cwd())
+    with jsonlines.open('./results.jsonl') as f:
+
+        for line in f.iter():
+
+            start_urls.append(line["pageLink"])
+    
+
+    def parse(self, response):    # Funcion called every crawled web page. The response parameter will contain the web site response.
+        target = response.xpath(".//div[@class='main-content']")    # For each quote element in the current page...
+
+            # text = quote.xpath(".//span[@class='text']/text()").get()                    # Extract the quote text as a string.
+                # author = quote.xpath(".//small[@class='author']/text()").get()	     # Extract the author name as a string.
+                # tags = quote.xpath(".//div[@class='tags']/a[@class='tag']/text()").getall()  # Extract the quote tags as a list of strings.
+        name = target.xpath(".//h1[@class='detail-desc-make-model']/text()").get()
+        transmission = target.xpath(".//li[@class='listing-bullet-transmission']/text()").get()
+        mileage = target.xpath(".//li[@class='detail-bullet-mileage']/text()").get()
+        drive = target.xpath(".//li[@class='listing-bullet-drive']/text()").get()
+
+        yield {"name" : name, "transmission" : transmission, "mileage" : mileage, "driver-position": drive}
+
+
+
+
+
--- project/classic_cars/classic_cars/spiders/link_scraper.py
+++ project/classic_cars/classic_cars/spiders/link_scraper.py
@@ -1,26 +1,26 @@
 import scrapy
 
 
-class CarSpider(scrapy.Spider):
+class linkSpider(scrapy.Spider):
 
-    name = "classicCars"    # Your spider name. Each instance of a QuoteSpider will share the same name.
+    name = "pagelinks"    # Your spider name. Each instance of a QuoteSpider will share the same name.
 
 
     start_urls = ["https://www.classiccarsforsale.co.uk/year-1885-2017"]
 
 
     def parse(self, response):    # Funcion called every crawled web page. The response parameter will contain the web site response.
-
         for car in response.xpath("//div[@class='listing-desc']"):    # For each quote element in the current page...
 
             # text = quote.xpath(".//span[@class='text']/text()").get()                    # Extract the quote text as a string.
-            # author = quote.xpath(".//small[@class='author']/text()").get()	     # Extract the author name as a string.
-            # tags = quote.xpath(".//div[@class='tags']/a[@class='tag']/text()").getall()  # Extract the quote tags as a list of strings.
-            year = car.xpath(".//div[@class='listing-desc-year']/text()").get()
-            price = car.xpath(".//div[@class='listing-desc-price']/text()").get()
-            milage = car.xpath(".//li[@class='listing-bullet-mileage']/text()").get()
-
-            yield {'year': year, 'price': price, 'milage': milage}    # Return extracted data as a Python dict.
+                # author = quote.xpath(".//small[@class='author']/text()").get()	     # Extract the author name as a string.
+                # tags = quote.xpath(".//div[@class='tags']/a[@class='tag']/text()").getall()  # Extract the quote tags as a list of strings.
+            link = response.urljoin(car.xpath("//div[@class='listing-desc-make-model']/a/@href").get())
+            yield {"pageLink" : link}
+        # print(carLinks)
+        next_page = response.xpath("//a[@class='btn nxt-link']/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
 
         # next_page = response.xpath("//li[@class='next']/a/@href").get()    # Extract next page link as a string.
 
