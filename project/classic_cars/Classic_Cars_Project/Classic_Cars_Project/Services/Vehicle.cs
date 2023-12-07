5816c72d6e2deee80f96be51ef9e376738b6f1f7
Georgy <87906732+BatyrGeo@users.noreply.github.com>
2023-12-07 15:21:45 +0100

    pagination

--- project/classic_cars/Classic_Cars_Project/Classic_Cars_Project/Services/Vehicle.cs
+++ project/classic_cars/Classic_Cars_Project/Classic_Cars_Project/Services/Vehicle.cs
@@ -4,10 +4,13 @@ namespace Classic_Cars_Project.Services
     public class Vehicle
     {
         public string ImageUrl { get; set; }
-        public string Model { get; set; }
+        public string Brand { get; set; }
+     
         public int Price { get; set; }
         public string Year { get; set; }
+
         public string DetailUrl { get; set; }
+
     }
 
 }
