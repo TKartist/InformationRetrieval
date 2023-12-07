501c7284bd3d868a517b3f07068c05f4fe5f98bf
TKartist <rimh@usi.ch>
2023-12-07 23:50:57 +0100

    restructuring

--- project/classic_cars/utilities/file_paths.py
+++ project/classic_cars/utilities/file_paths.py
@@ -1,7 +1,7 @@
 json_files = [
-    "./JSONS/contents_ccc.json",
-    "./JSONS/contents_erc.json",
-    "./JSONS/contents_ccs.json",
+    "../JSONS/contents_ccc.json",
+    "../JSONS/contents_erc.json",
+    "../JSONS/contents_ccs.json",
 ]
-DBPATH = "./JSONS/jsonDB.json"
-INDEXPATH = "./JSONS/indexJson.json"
+DBPATH = "../JSONS/jsonDB.json"
+INDEXPATH = "../JSONS/indexJson.json"
