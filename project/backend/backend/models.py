ce5ba9ad8d5fe4b8a9bb1f48a17b727da4ddc2ab
Georgy <87906732+BatyrGeo@users.noreply.github.com>
2023-12-10 13:07:52 +0100

    cosmetic push

--- project/backend/backend/models.py
+++ project/backend/backend/models.py
@@ -1,5 +1,6 @@
 from djongo import models
 
+
 class Vehicle(models.Model):
     brand = models.CharField(max_length=100)
     year = models.CharField(max_length=4)
@@ -9,6 +10,6 @@ class Vehicle(models.Model):
     image_url = models.URLField(max_length=200)
     detail_url = models.URLField(max_length=200)
     docno = models.CharField(max_length=4)
-    
+
     def __str__(self):
         return f"{self.brand} {self.year}"
--- project/backend/backend/views.py
+++ project/backend/backend/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator
 from .models import Vehicle
 from .serializers import VehicleSerializer
 
+
 class VehicleListView(ListAPIView):
     serializer_class = VehicleSerializer
 
@@ -12,24 +13,24 @@ class VehicleListView(ListAPIView):
         queryset = Vehicle.objects.all()
 
         # Retrieve query parameters
-        search_query = self.request.query_params.get('search', None)
-        brand = self.request.query_params.get('brand', None)
-        model = self.request.query_params.get('model', None)
-        year = self.request.query_params.get('year', None)
-        min_price = self.request.query_params.get('minPrice', None)
-        max_price = self.request.query_params.get('maxPrice', None)
-        page = self.request.query_params.get('currentPage', 1 )
-        items_per_page = self.request.query_params.get('itemsPerPage', 20)
+        search_query = self.request.query_params.get("search", None)
+        brand = self.request.query_params.get("brand", None)
+        model = self.request.query_params.get("model", None)
+        year = self.request.query_params.get("year", None)
+        min_price = self.request.query_params.get("minPrice", None)
+        max_price = self.request.query_params.get("maxPrice", None)
+        page = self.request.query_params.get("currentPage", 1)
+        items_per_page = self.request.query_params.get("itemsPerPage", 20)
 
         # Apply filters if parameters are present
         if search_query:
-            queryset = queryset.filter(text__icontains=search_query )
-        
+            queryset = queryset.filter(text__icontains=search_query)
+
         if model:
             queryset = queryset.filter(model__icontains=model)
-        
+
         if brand:
-            queryset = queryset.filter(brand__iexact=brand) 
+            queryset = queryset.filter(brand__iexact=brand)
         if year:
             queryset = queryset.filter(year=year)
         if min_price is not None:
@@ -37,6 +38,5 @@ class VehicleListView(ListAPIView):
         if max_price is not None:
             queryset = queryset.filter(price__lte=max_price)
 
-    
         paginator = Paginator(queryset, items_per_page)
         return paginator.get_page(page).object_list
